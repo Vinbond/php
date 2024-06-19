93c1989f3e51f791ba5a0d5fa6f009fa1c02b659
vinbond <vinayakkundangar77@gmail.com>
2024-06-19 18:23:31 +0530

    First

---
+++ datatypes.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<?php
+        $hhh=123;
+        #Data Types in PHP
+        //Integer 
+        //var_dump($int);
+         echo $hhh;
+
+
+
+
+?>
+</body>
+</html>
---
+++ index.php
@@ -0,0 +1,52 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+    $name="Hello";
+    echo $name."</br>";
+    /* Data Types In PHP.We Have 8 Data types in php lets see one by one*/
+    // $int_cont=123;
+    // $str_ng="This Is Known as String";
+    // $flt_cont=123.90;
+    // $bln_cont=TRUE;
+    // $arr=array('H','T','Y');
+    //  echo $arr;
+    // $nn_cont=null;
+    // class $obj= new Car();
+    // $rr=fopen("text.txt","r");
+    // var_dump($int_cont);
+    // var_dump($str_ng);
+    // var_dump($flt_cont);
+    // var_dump($bln_cont);
+    // var_dump($arr);
+    // var_dump($nn_cont);
+    // var_dump($rr);
+    //String Concatenation Using . OR =
+    // $str1="Hello";
+    // $str2="Bye";
+    // $str3=$str1.''.$str2;
+    // echo $str3;
+//    $a=12;
+//     $a++;
+//     echo $a."<br>";
+
+//     $c=--$a;
+//     echo $c."<br>";
+// class Car
+//  $carobj = new Car()
+
+
+
+
+    
+    
+    
+    ?>
+    
+</body>
+</html>
